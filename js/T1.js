974dc3c9a288c68deb35b74ca046d17ea1ad3a68
Konrad Werys <konradwerys2@gmail.com>
2019-11-02 12:10:58 +0100

    fixing js

--- js/T1.js
+++ js/T1.js
@@ -2,6 +2,8 @@ var invTimes = [  100,  180,  260, 1715, 3337, 4907, 6455];
 var blood    = [  -55,  -49,  -29,   22,   51,   61,   64];
 var myo      = [  -49,  -40,  -26,   42,   59,   62,   63];
 
+console.log('a')
+
 var dataBlood = new Array(invTimes.length);
 var dataMyo = new Array(invTimes.length);
 
@@ -14,12 +16,12 @@ for (i = 0; i < invTimes.length; i++) {
 var datasetBlood = { label: 'Blood', data: dataBlood, fill: false, borderColor: "#c45850" };
 var datasetMyo = { label: 'Myocardium', data: dataMyo, fill: false, borderColor: "#3cba9f" };
 
-var xAxes = { type: 'linear', scaleLabel: { display: true, labelString: 'Inversion time' } };
-var yAxes = { type: 'linear', scaleLabel: { display: true, labelString: 'Magnetisation' } };
+var myxAxes = [{ type: 'linear', scaleLabel: { display: true, labelString: 'Inversion time' } }];
+var myyAxes = [{ type: 'linear', scaleLabel: { display: true, labelString: 'Magnetisation' } }];
 
 var ctx = document.getElementById("chartT1");
 var scatterChart = new Chart(ctx, {
   type: 'line',
   data: { datasets: [datasetBlood, datasetMyo] },
-  options: { scales: { xAxes: xAxes, yAxes: yAxes }}
+  options: { scales: { xAxes: myxAxes, yAxes: myyAxes }}
 });
